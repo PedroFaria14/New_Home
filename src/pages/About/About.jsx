d4f03cdd9a2fabca7fa0113e3b388cb85cafa8ce
Pedro Henrique de Faria <99847050+PedroFaria14@users.noreply.github.com>
2024-12-26 18:28:59 -0300

    Update About.jsx

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -9,7 +9,7 @@ const About = () => {
         Sobre o New <span>Home</span>
       </h2>
       <p>
-      O projeto é um site especializado na venda de propriedades, construído com React e JavaScript, integrado ao Firebase para armazenamento e gerenciamento de dados. Essa plataforma proporciona um ambiente dinâmico e interativo para artistas e entusiastas de design compartilharem suas criações e experiências.
+        Tenho 19 anos e estou cursando Engenharia da Computação na USF e Análise e Desenvolvimento de Sistemas na UNICSUL, ambos no 3º semestre. Sou formado em Técnico em Informática pela ETEC e atualmente estagio na área de Infraestrutura de TI. Tenho grande interesse na área de desenvolvimento e busco aprender Java, React e Python para fortalecer minhas habilidades como desenvolvedor, criando soluções inovadoras e impactantes. Procuro uma oportunidade de estágio na área de desenvolvimento para aplicar e expandir meus conhecimentos, contribuindo para projetos desafiadores e de alto impacto.    
       </p>
       <Link to="/posts/create" className="btn">
         Criar post
